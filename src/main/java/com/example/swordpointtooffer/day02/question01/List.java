d391c9c6e6ac65d1872075714d50f6c596d332ba
PlutoCtx <chentingxian195467@163.com>
2023-03-27 23:39:32 +0800

    ignore this

--- src/main/java/com/example/swordpointtooffer/day02/question01/List.java
+++ src/main/java/com/example/swordpointtooffer/day02/question01/List.java
@@ -43,15 +43,5 @@ public class List {
         return res;
     }
 
-
-
 }
 
-/*
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
